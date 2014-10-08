b21bf6f9019a7d9acb97a944a81f01eff8285342
jiading <jiading@jiading-h8-1280t.(none)>
2014-10-08 12:36:55 -0500

    Turn off gdb in drive_all.sh

--- sroa_history/drive_all.sh
+++ sroa_history/drive_all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 make
-gdb --args opt -S -load ./mysroa.so -mysroa -debug-only=MYSROA test/2014-09-23-gai.ll 
-#opt -S -load ./mysroa.so -mysroa -debug-only=MYSROA < test/2014-09-23-gai.ll
+#gdb --args opt -S -load ./mysroa.so -mysroa -debug-only=MYSROA test/2014-09-23-gai.ll 
+opt -S -load ./mysroa.so -mysroa -debug-only=MYSROA < test/2014-09-23-gai.ll
 gcov mysroa.cpp
