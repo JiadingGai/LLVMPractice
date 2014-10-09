31f327a6ad10d0b823e30cfba2b1bd7b17879ff0
jiading <paul.paul.mit@gmail.com>
2014-10-09 14:54:30 -0500

    Turn off the gcov call in drive_all.sh

--- sroa_history/drive_all.sh
+++ sroa_history/drive_all.sh
@@ -3,4 +3,4 @@ make
 #gdb --args opt -S -load ./mysroa.so -mysroa -debug-only=MYSROA test/2014-09-23-gai.ll 
 #opt -S -load ./mysroa.so -mysroa -debug-only=MYSROA < test/2014-09-23-gai.ll
 opt -S -load ./mysroa.so -mysroa -debug-only=MYSROA < test/arraytest.ll
-gcov mysroa.cpp
+#gcov mysroa.cpp
